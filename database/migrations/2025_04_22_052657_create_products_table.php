35434f815ecb5e1a811342e219207fdfb8996c2a
Chimaobi Anthony <102151079+obahchimaobi@users.noreply.github.com>
2025-04-22 09:39:15 +0100

    Table For Products

--- database/migrations/2025_04_22_052657_create_products_table.php
+++ database/migrations/2025_04_22_052657_create_products_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,8 +14,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug');
-            $table->string('primary_image')->nullable();
-            $table->string('secondary_image')->nullable();
+            $table->string('image')->nullable();
             $table->decimal('price', 10, 2)->nullable();
             $table->decimal('original_price', 10, 2)->nullable();
             $table->integer('discount_percentage')->nullable();
@@ -25,6 +23,10 @@ return new class extends Migration
             $table->integer('rating_count')->nullable()->default(0);
             $table->boolean('status')->default(false);
             $table->longText('description')->nullable();
+            $table->string('sku')->nullable()->unique();
+            $table->integer('stock_quantity')->nullable();
+            $table->enum('stock_status', ['in_stock', 'out_of_stock', 'pre_order'])->default('in_stock')->nullable();
+            $table->timestamp('deleted_at')->nullable();
             $table->timestamps();
         });
     }
@@ -34,6 +36,8 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('product_images');
+        Schema::dropIfExists('product_variants');
         Schema::dropIfExists('products');
     }
 };
