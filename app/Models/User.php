3b2d0119b88d6b47de583289f7d3807642a2cc68
Chimaobi Anthony <102151079+obahchimaobi@users.noreply.github.com>
2025-04-22 05:50:46 +0100

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,8 @@ class User extends Authenticatable
         'email',
         'password',
         'token',
-        'verification_code'
+        'verification_code',
+        'is_admin',
     ];
 
     /**
