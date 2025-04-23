a535b82f9b2e27181e6032acfb84cb3346ffebbd
Chimaobi Anthony <102151079+obahchimaobi@users.noreply.github.com>
2025-04-23 22:43:50 +0100

    Initial Commit

---
+++ app/Livewire/Home/NewArrivalsSection.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire\Home;
+
+use App\Models\Products;
+use Livewire\Component;
+
+class NewArrivalsSection extends Component
+{
+    public function render()
+    {
+        $women_arrivals = Products::where('category_type', 'Women')->paginate(10);
+        $men_arrivals = Products::where('category_type', 'Men')->paginate(10);
+        
+        return view('livewire.home.new-arrivals-section', compact('women_arrivals', 'men_arrivals'));
+    }
+}
