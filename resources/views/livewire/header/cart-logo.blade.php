8d29886c82bd149de66e721183b36d2280e54c5f
Chimaobi Anthony <102151079+obahchimaobi@users.noreply.github.com>
2025-04-30 15:00:59 +0100

    Working on resources\views\livewire\header\cart-logo.blade.php '' app/livewire/header/CartLogo.php

--- resources/views/livewire/header/cart-logo.blade.php
+++ resources/views/livewire/header/cart-logo.blade.php
@@ -9,68 +9,43 @@
         <!--Minicart Popup-->
         <div id="header-cart" class="block block-cart">
             <ul class="mini-products-list">
-                <li class="item">
-                    <a class="product-image" href="{{ asset('#') }}">
-                        <img src="{{ asset('assets/images/product-images/cape-dress-1.jpg') }}"
-                            alt="3/4 Sleeve Kimono Dress" title="" />
-                    </a>
-                    <div class="product-details">
-                        <a href="{{ asset('#') }}" class="remove"><i class="anm anm-times-l"
-                                aria-hidden="true"></i></a>
-                        <a href="{{ asset('#') }}" class="edit-i remove"><i class="anm anm-edit"
-                                aria-hidden="true"></i></a>
-                        <a class="pName" href="{{ asset('cart.html') }}">Sleeve Kimono
-                            Dress</a>
-                        <div class="variant-cart">Black / XL</div>
-                        <div class="wrapQtyBtn">
-                            <div class="qtyField">
-                                <span class="label">Qty:</span>
-                                <a class="qtyBtn minus" href="{{ asset('javascript:void(0);') }}"><i
-                                        class="fa anm anm-minus-r" aria-hidden="true"></i></a>
-                                <input type="text" id="Quantity" name="quantity" value="1"
-                                    class="product-form__input qty">
-                                <a class="qtyBtn plus" href="{{ asset('javascript:void(0);') }}"><i
-                                        class="fa anm anm-plus-r" aria-hidden="true"></i></a>
+                @if ($get_cart)
+                    @foreach ($get_cart as $cart)
+                        <li class="item">
+                            <a class="product-image" href="{{ asset('#') }}">
+                                <img src="{{ asset('storage/' . $cart->product_image) }}" alt="3/4 Sleeve Kimono Dress"
+                                    title="" />
+                            </a>
+                            <div class="product-details">
+                                <a href="{{ asset('#') }}" class="remove"><i class="anm anm-times-l"
+                                        aria-hidden="true"></i></a>
+                                <a class="pName" href="{{ asset('cart.html') }}">{{ $cart->products->name }}</a>
+                                <div class="variant-cart">
+                                    @if (!is_null($cart->color))
+                                        {{ $cart->color }} /
+                                    @endif @if (!is_null($cart->size))
+                                        {{ $cart->size }}
+                                    @endif</div>
+                                <div class="wrapQtyBtn">
+                                    <div class="qtyField">
+                                        <span class="label">Qty:</span>
+                                        <a class="qtyBtn minus" href="{{ asset('javascript:void(0);') }}"><i
+                                                class="fa anm anm-minus-r" aria-hidden="true"></i></a>
+                                        <input type="text" id="Quantity" name="quantity" value="{{ $cart->quantity }}"
+                                            class="product-form__input qty">
+                                        <a class="qtyBtn plus" href="{{ asset('javascript:void(0);') }}"><i
+                                                class="fa anm anm-plus-r" aria-hidden="true"></i></a>
+                                    </div>
+                                </div>
+                                <div class="priceRow">
+                                    <div class="product-price">
+                                        <span class="money">$59.00</span>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="priceRow">
-                            <div class="product-price">
-                                <span class="money">$59.00</span>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li class="item">
-                    <a class="product-image" href="{{ asset('#') }}">
-                        <img src="{{ asset('assets/images/product-images/cape-dress-2.jpg') }}"
-                            alt="Elastic Waist Dress - Black / Small" title="" />
-                    </a>
-                    <div class="product-details">
-                        <a href="{{ asset('#') }}" class="remove"><i class="anm anm-times-l"
-                                aria-hidden="true"></i></a>
-                        <a href="{{ asset('#') }}" class="edit-i remove"><i class="anm anm-edit"
-                                aria-hidden="true"></i></a>
-                        <a class="pName" href="{{ asset('cart.html') }}">Elastic Waist
-                            Dress</a>
-                        <div class="variant-cart">Gray / XXL</div>
-                        <div class="wrapQtyBtn">
-                            <div class="qtyField">
-                                <span class="label">Qty:</span>
-                                <a class="qtyBtn minus" href="{{ asset('javascript:void(0);') }}"><i
-                                        class="fa anm anm-minus-r" aria-hidden="true"></i></a>
-                                <input type="text" id="Quantity" name="quantity" value="1"
-                                    class="product-form__input qty">
-                                <a class="qtyBtn plus" href="{{ asset('javascript:void(0);') }}"><i
-                                        class="fa anm anm-plus-r" aria-hidden="true"></i></a>
-                            </div>
-                        </div>
-                        <div class="priceRow">
-                            <div class="product-price">
-                                <span class="money">$99.00</span>
-                            </div>
-                        </div>
-                    </div>
-                </li>
+                        </li>
+                    @endforeach
+                @endif
             </ul>
             <div class="total">
                 <div class="total-in">
